3e382d80dbc9208db32ab803b51aa3f35062a187
u-gohil <756D6D61@gmail.com>
2020-06-29 18:30:42 +0100

    jpa

--- src/main/java/com/sbproject/firstProject/Model/Person.java
+++ src/main/java/com/sbproject/firstProject/Model/Person.java
@@ -12,26 +12,32 @@ public class Person {
     }
 
     public String getFirstName() {
+
         return firstName;
     }
 
     public void setFirstName(String firstName) {
+
         this.firstName = firstName;
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
     public void setLastName(String lastName) {
+
         this.lastName = lastName;
     }
 
     public String getAge() {
+
         return age;
     }
 
     public void setAge(String age) {
+
         this.age = age;
     }
 
